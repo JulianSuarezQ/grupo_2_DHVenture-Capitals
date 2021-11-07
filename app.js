e19e5743bf7ef65657fafd248caa3652f36bdf62
gonzalopez-ml <gonzalo.glopez@mercadolibre.com>
2021-11-07 16:30:53 -0300

    refactor

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const port = 3000;
 
-const express = require('express');
+const express = require("express");
 const path = require("path");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath));
-
-
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
 app.listen(port, () => {
-  console.log ('Servidor corriendo en el puerto' + ' ' + port)
+  console.log("Servidor corriendo en el puerto" + " " + port);
 });
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 
-app.get('/login.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
-})
+app.get("/login.html", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
