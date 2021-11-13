581879047d3e69500ce11fc416e721a66adc3c25
FacuTaborra <taborrafacundo@gmail.com>
2021-11-13 14:20:46 -0300

    primera version de register

--- app.js
+++ app.js
@@ -19,3 +19,7 @@ app.get("/", (req, res) => {
 app.get("/login.html", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
+
+app.get("/register.html", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
