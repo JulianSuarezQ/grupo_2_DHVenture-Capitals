752e18df6bfd8bc261bc6c47eca4131f6cd71726
JulianSuarezQ <juliansuarezq@hotmail.com>
2021-11-24 21:25:19 -0300

    motor de templates agregado

---
+++ src/routes/main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const mainController = require('../controllers/mainController');
+
+
+router.get('/', mainController.index);
+router.get('/carrito', mainController.carrito);
+router.get('/login', mainController.login);
+router.get('/register', mainController.register);
+router.get('/producto-1', mainController.producto1);
+
+
+
+module.exports = router
