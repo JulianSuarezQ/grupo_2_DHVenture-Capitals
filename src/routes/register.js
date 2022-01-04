4763c30f350440971d5d349d7c38435aa36107ed
FacuTaborra <taborrafacundo@gmail.com>
2022-01-04 20:15:27 -0300

    askda

--- src/routes/register.js
+++ src/routes/register.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const registerController = require('../controllers/registerController');
 
 router.get('/', registerController.register);
+router.post('/', registerController.createUser);
 
 module.exports = router;
