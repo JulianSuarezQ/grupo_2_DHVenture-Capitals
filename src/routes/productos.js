96079557b53b3253a367a5dc803971f32a26c65c
gonzalopez-ml <gonzalo.glopez@mercadolibre.com>
2022-03-14 21:33:58 -0300

    se agrega middleware producto para validar backend

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -3,14 +3,20 @@ const router = express.Router();
 const upload = require("../middlewares/multer");
 const productosController = require("../controllers/productosController");
 const userLogged = require("../middlewares/userLoggedMiddleware");
-
+const validateCreateProduct = require("../middlewares/createProductMiddleware");
 
 // '/products/....'
 
- router.get("/", userLogged, productosController.list); 
+router.get("/", userLogged, productosController.list);
 
 router.get("/create", userLogged, productosController.create);
-router.post("/create",userLogged,upload.single("img"), productosController.store);
+router.post(
+  "/create",
+  userLogged,
+  upload.single("img"),
+  validateCreateProduct,
+  productosController.store
+);
 
 router.get("/search", userLogged, productosController.search);
 
@@ -19,7 +25,12 @@ router.get("/description", userLogged, productosController.descriptionProduct);
 router.get("/:id", userLogged, productosController.detail);
 
 router.get("/edit/:id", userLogged, productosController.edit);
-router.put("/edit/:id",userLogged,upload.single("img"),productosController.update);
+router.put(
+  "/edit/:id",
+  userLogged,
+  upload.single("img"),
+  productosController.update
+);
 
 router.get("/delete/:id", userLogged, productosController.PagDelete);
 router.post("/delete/:id", userLogged, productosController.delete);
