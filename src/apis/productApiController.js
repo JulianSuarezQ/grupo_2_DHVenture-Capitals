fd6602fe285a481a02a0fbdc2688ef3c268e36de
EstebanKa <estebankarlenaguirre@gmail.com>
2022-03-18 17:43:10 -0300

    problema de llave

--- src/apis/productApiController.js
+++ src/apis/productApiController.js
@@ -42,11 +42,47 @@ const productApiController = {
     let todasCategorias = db.Category.findAll();
              Promise.all([todosProductos, todasCategorias])
             .then(function([products , categoriaMostrar]){
-            res.status(200).json({
-                data:products, categoriaMostrar
-          });
+                res.status(200).json({
+                    meta:{
+                      status: 200,
+                      cant: products.length, 
+                      url: 'api/products'
+                    },
+              });
         });
       },
-}
+      
+      productById: function (req, res) {
+        db.Products.findByPk(req.params.id, {
+          include: [
+            {
+              association: "category",
+            },
+          ],
+        })
+          .then((product) => {
+            return res.status(200).json({
+              id: product.id,
+              name: product.name,
+              discount: product.discount,
+              detail: product.detail,
+              stock: product.stock,
+              category: product.category.name,
+              color: product.color,
+              price: product.price,
+              size: product.size,
+              img: product.img,
+              status: 200,
+            });
+          })
+          .catch((error) => {
+            error =
+              "El ID " + req.params.id + " no existe en nuestra base de datos";
+            return res.status(404).json({
+              error,
+            });
+          });
+      },
+};
 
 module.exports = productApiController;
