677814427564e1696f2edb5231f282e18bd62a1b
FacuTaborra <taborrafacundo@gmail.com>
2022-03-19 19:38:33 -0300

    haciendo apis

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: "root",
-    password: "",
+    password: "facu2323",
     database: "DB_Scaloneta",
     host: "127.0.0.1",
     dialect: "mysql",
