75c0bdff8a2d2e6d36ac2d29df93557cffc1125e
gonzalopez-ml <gonzalo.glopez@mercadolibre.com>
2022-03-22 16:16:47 -0300

    css edit form

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
-    username: "root",
-    password: "facu2323",
+    username: "admina",
+    password: "admin",
     database: "DB_Scaloneta",
     host: "127.0.0.1",
     dialect: "mysql",
